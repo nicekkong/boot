7f486682cf979b8e702257e7292d688fcb3ac65c
nicekkong <nicekkong@gmail.com>
2021-04-18 00:59:25 +0900

    external tomcat

--- src/main/java/com/nicekkong/boot/BootApplication.java
+++ src/main/java/com/nicekkong/boot/BootApplication.java
@@ -2,12 +2,17 @@ package com.nicekkong.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
+    }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BootApplication.class);
     }
 
 }
